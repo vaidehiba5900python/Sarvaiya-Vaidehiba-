0809975b91c6f4ca8af6bd0bd21cdd176cc25386
vaidehiba5900python <vaidehiba5900@gmail.com>
2026-02-23 18:19:52 +0530

    Add files via upload

---
+++ Corrected overtime Calculator.py
@@ -0,0 +1,26 @@
+print("Overtime Calculator")
+workers = int(input("Enter number of workers: "))
+i = 0
+total_payment = 0
+
+while i < workers:
+    hours = int(input("Enter working hours: "))
+    rate = int(input("Enter rate per hour: "))
+
+    if hours > 40:
+        overtime = (hours - 40) * rate * 1.5
+    else:
+        overtime = 0
+
+    salary = (40 * rate) + overtime
+    total_payment = total_payment + salary
+
+    print("Salary:", salary)
+    i = i + 1
+
+print("Total Payment:", total_payment)
+
+if total_payment > 100000:
+    print("High expense")
+else:
+    print("Normal expense")
